685166e2d74a0972e964a309b45f553ef0bd67dc
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2016-02-23 18:46:48 +0000

    serve basic html page for admin console (doesn't do anything yet)

--- proxy.js
+++ proxy.js
@@ -1,12 +1,16 @@
 var http = require('http');
 fs = require('fs');
 
-const PORT = 8080;
+const PPORT = 8080;
+const APORT = 3000;
+const BLACKLIST_PATH = 'blocked.txt';
+const HTML_PATH = 'index.html';
 
-function handleRequest(request, response) {
+
+function handleProxyRequest(request, response) {
   console.log("url: " + request.url);
   console.log("host: " + request.headers['host']);
-  fs.readFile('blocked.txt', 'utf8', function (err,data) {
+  fs.readFile(BLACKLIST_PATH, 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
@@ -39,4 +43,21 @@ function handleRequest(request, response) {
   });
 }
 
-http.createServer(handleRequest).listen(PORT);
+function handleAdminRequest(request, response) {
+  fs.readFile(HTML_PATH, 'utf8', function (err,data) {
+    if (err) {
+      response.writeHead(404);
+      response.write("404 - Not found");
+      console.log(err);
+    } 
+    else {
+      response.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
+      response.write(data);
+    }
+    response.end();
+  });
+}
+
+http.createServer(handleProxyRequest).listen(PPORT);
+
+http.createServer(handleAdminRequest).listen(APORT);
